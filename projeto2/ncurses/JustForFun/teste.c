b5f515f7e1f12e34c494aa4fe4767e59f4040fc4
Joao-Pedro-MB <joaopedreobonucci@gmail.com>
2022-05-08 22:41:03 +0000

    improved animation code

--- projeto2/ncurses/JustForFun/teste.c
+++ projeto2/ncurses/JustForFun/teste.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void show_status (int progress, char* passenger_1, char* passenger_2, char* passenger_3, char* passenger_4,int* hackers,int* devs) {
+void makeWaitingLine(int * hackers, int * serfs, char * hackers_str, char * serfs_str) {
+    for (int i = 0 ; hackers[i] != NULL ; i++) {
+        if (hackers[i] < 10) {
+            hackers_str += 'H' + '0' +  (char) hackers[i];
+        } else {
+            hackers_str += 'H' + (char) hackers[i];
+        }
+    }
+
+    for (int i = 0 ; serfs[i] != NULL ; i++) {
+        if (serfs[i] < 10) {
+            serfs_str += 'H' + '0' +  (char) serfs[i];
+        } else {
+            serfs_str += 'H' + (char) serfs[i];
+        }
+    }
+
+    hackers_str += '\n';
+    serfs_str += '\n';
+
+}
+
+void show_status (int progress, char* passenger_1, char* passenger_2, char* passenger_3, char* passenger_4,int* hackers,int* serfs) {
+
+    char  hackers_str[50], serfs_str[50];
+    makeMaikingLine(hackers, serfs, hackers_str, serfs_str);
 
     system("clear");
     printf("\n");
     printf("\n");
     printf("Hackers\n");
-    printf("H01 H02 H03 \n");
+    printf(hackers);
     printf("\n");
     printf("Devs\n");
-    printf("S01 S02 S03 \n");
+    printf(serfs);
 
     for(int x = 0; x < progress; x++) {   
         printf("=");
