3083fecf3c57285ac5708b2c9e787b4196cb92db
Joao-Pedro-MB <joaopedreobonucci@gmail.com>
2022-05-08 22:24:29 +0000

    teste

--- projeto2/ncurses/JustForFun/teste.c
+++ projeto2/ncurses/JustForFun/teste.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void show_status (
-    int progress, 
-    char* passenger_1, 
-    char* passenger_2, 
-    char* passenger_3, 
-    char* passenger_4,
-    int* hackers,
-    int* devs,
-    ) {
+void show_status (int progress, char* passenger_1, char* passenger_2, char* passenger_3, char* passenger_4,int* hackers,int* devs) {
 
     system("clear");
     printf("\n");
@@ -23,7 +15,7 @@ void show_status (
     for(int x = 0; x < progress; x++) {   
         printf("=");
     }
-        printf("<_%s_%s_´T`_%s_%s_>\r", passenger_1, passenger_2, passenger_3, passenger_4);
+        printf("\\_%s_%s_´T`_%s_%s_/\r", passenger_1, passenger_2, passenger_3, passenger_4);
         system("sleep 1");   
     
 }
@@ -31,9 +23,10 @@ void show_status (
 int main() {
 
     int times = 10;
+    int oi[10] = {0,1,2,3,4,5,6,7,8,9};
          
     for(int i = 0; i <= times; i++) {
-       show_status(i * 8, "H1", "S2", "H3", "S4");
+       show_status(i * 8, "H1", "S2", "H3", "S4", oi, oi);
     }
     
     
