ba30d95111ad458af54a7379161c43e9ef2cfccc
Pedro <pedrodearaujo3001@gmail.com>
2022-05-08 22:03:40 -0300

    adding boat animation

---
+++ projeto2/ncurses/JustForFun/teste.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void show_status (
+    int progress, 
+    char* passenger_1, 
+    char* passenger_2, 
+    char* passenger_3, 
+    char* passenger_4,
+    int* hackers,
+    int* devs,
+    ) {
+
+    system("clear");
+    printf("\n");
+    printf("\n");
+    printf("Hackers\n");
+    printf("H01 H02 H03 \n");
+    printf("\n");
+    printf("Devs\n");
+    printf("S01 S02 S03 \n");
+
+    for(int x = 0; x < progress; x++) {   
+        printf("=");
+    }
+        printf("<_%s_%s_´T`_%s_%s_>\r", passenger_1, passenger_2, passenger_3, passenger_4);
+        system("sleep 1");   
+    
+}
+
+int main() {
+
+    int times = 10;
+         
+    for(int i = 0; i <= times; i++) {
+       show_status(i * 8, "H1", "S2", "H3", "S4");
+    }
+    
+    
+   return (EXIT_SUCCESS);    
+    
+}
